8b648961ce7e01194fcb1a842acb898a6468a419
Oscar Cheung <hycheung0509@gmail.com>
2015-03-02 15:41:36 +0800

    Add or remove class of body tag when the icon clicked

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-document.body.classList.add('chrome-webkit-font-smoothing-switch');
+var isTurnedOn = false;
+var injectClass = function() {
+    var codeToBeInjected = isTurnedOn ? 'document.body.classList.add("chrome-webkit-font-smoothing-switch")' :
+            'document.body.classList.remove("chrome-webkit-font-smoothing-switch")';
+    chrome.tabs.executeScript({
+        code: codeToBeInjected
+    });
+};
+
+chrome.browserAction.onClicked.addListener(function browserActionOnClickHandler() {
+    isTurnedOn = !isTurnedOn;
+    injectClass();
+});
