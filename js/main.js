61ea9cc14e4dcaf9e6f70eda7a93e1a461c45528
Oscar Cheung <oscar@Oscars-MacBook-Pro.local>
2016-01-15 18:37:08 +0800

    Fix bug that crashes when closing numbers of tabs
    Bug caused by onActivated handling, and resolved by checking tab.status

--- js/main.js
+++ js/main.js
@@ -1,20 +1,33 @@
-var isTurnedOn = false;
-var injectClass = function() {
-    if (!chrome || !chrome.tabs || !chrome.tabs.executeScript) {
-        return;
-    }
+(function() {
+    var isTurnedOn = false;
+    var injectClass = function() {
+        var codeToBeInjected = isTurnedOn ? 'document.body.classList.add("chrome-webkit-font-smoothing-switch")' :
+                'document.body.classList.remove("chrome-webkit-font-smoothing-switch")';
+        chrome.tabs.executeScript({
+            code: codeToBeInjected
+        });
+    };
 
-    var codeToBeInjected = isTurnedOn ? 'document.body.classList.add("chrome-webkit-font-smoothing-switch")' :
-            'document.body.classList.remove("chrome-webkit-font-smoothing-switch")';
-    chrome.tabs.executeScript({
-        code: codeToBeInjected
+    chrome.browserAction.onClicked.addListener(function browserActionOnClickHandler() {
+        isTurnedOn = !isTurnedOn;
+        injectClass();
     });
-};
 
-chrome.browserAction.onClicked.addListener(function browserActionOnClickHandler() {
-    isTurnedOn = !isTurnedOn;
-    injectClass();
-});
+    chrome.tabs.onUpdated.addListener(function tabOnUpdatedHandler(changeInfo) {
+        var status = changeInfo.status;
+        if (status === 'complete') {
+            injectClass();
+        }
+    });
 
-chrome.tabs.onUpdated.addListener(injectClass);
-chrome.tabs.onActivated.addListener(injectClass);
+    chrome.tabs.onActivated.addListener(function tabOnActivatedHandler(activeInfo) {
+        var tabId = activeInfo.tabId
+        var getTabCompletedHandler = function(tab) {
+            if (tab.status === 'complete') {
+                injectClass();
+            }
+        };
+
+        chrome.tabs.get(tabId, getTabCompletedHandler);
+    });
+})();
